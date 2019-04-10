a72be1e4f098ebb575d5dc613cf30d8913810843
HABTAMU ASFAW <asfawh@uw.edu>
2019-04-09 17:23:58 -0700

    Initial commit

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY').encode()
 
 @app.route('/')
 def home():
@@ -15,9 +16,20 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+
+    if request.method == 'POST':
+        donor = Donor(name=request.form['name'])
+        donation = Donation(name=request.form['value'])
+        donor.save()
+
+        return redirect(url_for('all'))
+    else:
+        return render_template('create.jinja2')
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
-
